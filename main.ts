433d92ef8832290228dde83932f946239e2520aa
Jomacla <73792627+Jomacla@users.noreply.github.com>
2021-08-19 17:39:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,18 @@ radio.onReceivedString(function (receivedString) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 255)
     } else if (receivedString == "backward") {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 255)
-    } else if (receivedString == "") {
-    	
+    } else if (receivedString == "left") {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 127)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 127)
+    } else if (receivedString == "right") {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 127)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 127)
+    } else if (receivedString == "up") {
+        DFRobotMaqueenPlus.servoRun(Servos.S2, 0)
+    } else if (receivedString == "down") {
+        DFRobotMaqueenPlus.servoRun(Servos.S2, 90)
     } else {
-    	
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
 })
 radio.setGroup(1)
